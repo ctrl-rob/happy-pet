1f03236ac5694136b81a2b73ab341d04961a2e68
Ctrl-Rob <rob.jacq@protonmail.com>
2026-04-24 20:47:34 +0200

    Up

--- Toneko/app/injection.tsx
+++ Toneko/app/injection.tsx
@@ -1,15 +1,18 @@
-import { StyleSheet, Pressable, Text, View } from "react-native";
+import {TextInput, StyleSheet, Pressable, Text, View } from "react-native";
 import FontAwesome6 from '@expo/vector-icons/FontAwesome6';
 
 
-export const Injection = (props: { unitInsuline: number; decreaseUnit: () => void; increaseUnit: () => void }) => {
+ export const Injection = (props: { unitInsuline: number; decreaseUnit: () => void; increaseUnit: () => void }) => {
 
     return (
         <View style={styles.container}>
             <Pressable onPress={props.decreaseUnit}>
                 <FontAwesome6 name="square-minus" size={36} color="black" />
             </Pressable>
-            <Text style={{ marginHorizontal: 10, width: 50, textAlign: "center" }}>{props.unitInsuline}</Text>
+            <TextInput
+                style={styles.inputText}
+                value={props.unitInsuline.toString()}
+            />
             <Pressable onPress={props.increaseUnit}>
                 <FontAwesome6 name="square-plus" size={36} color="black" />
             </Pressable>
@@ -21,5 +24,15 @@ const styles = StyleSheet.create({
     container: {
         flexDirection: "row",
         alignItems: "center"
+    },
+    inputText: {
+        textAlign: "center",
+        borderWidth: 2,
+        marginHorizontal: 4,
+        borderRadius: "10px",
+        lineHeight: "1.5rem",
+        
     }
 })
+
+ 
